d028d7e5cc75677bc66fdd696060d76d22a7c636
Steven Jackson <hpats67@gmail.com>
2017-05-07 11:31:08 -0700

    refactored math code

--- class-05-images-color-text/lab/app.js
+++ class-05-images-color-text/lab/app.js
@@ -1,173 +1,49 @@
 'use strict';
 
-///////////////////////////////////// 
-/* Problem 1 (this is your demo that we'll solve in class)
 
-Write a function called sum() that takes in two numbers as
-arguments and then returns an array where the first element 
-is the sum of those numbers, and the second element is a concatenated
-string that EXACTLY follows this example and uses 
-the values that were input into the function:
-
-"The sum of 4 and 7 is 11."
-
-Test this function by hand in the console to get it working,
-and when you think it is finished, uncomment the call
-for the testSum() function below and check the console 
-to see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the 
-code between GitHub and your laptop. Don't forget to create 
-a new branch for your work on the next question!
-
-*/
-
-// Write your code here
-function sum(a,b){ //eslint-disable-line
-    var sum = a + b;
-    var str = "The sum of " + a + " and " + b + " is " + sum + ".";
-    
-    return [ sum, str ];
-
-    // return [ sum, "The sum of " + a + " and " + b + " is " + sum + "."];
+function proAdd(opp, ...args){
+    return opp === '*' ? args.reduce((x,y) => x * y) : args.reduce((x,y) => x +y);
 }
 
-// sum (1, 2); // [ 3, "The sum of 1 and 2 is 3."]
-
-// Here is the test for sum(); uncomment it to run it
+// Write your code here
+function sum(a, b){ //eslint-disable-line
+    let ans = proAdd('+', a, b);
+    return [ans, `The sum of ${a} and ${b} is ${ans}.`];
+}
 testSum(4, 7);
-
-
-
-
-/////////////////////////////////////
-/* Problem 2
-Write a function called multiply() that takes in two numbers 
-as arguments and returns an array where the first element is the 
-product of those numbers, and the second element is a string 
-that EXACTLY follows this example and uses the values that 
-were input into the function:
-
-"The product of 5 and 9 is 45."
-
-Test this function by hand in the console to get it working,
-and when you think it is finished, uncomment the call for the 
-testMultiply() function and see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the code
-between GitHub and your laptop. Don't forget to 
-create a new branch for your work on the next question!
-*/
 
 // Write your code here
 function multiply(a,b){ //eslint-disable-line
-
+    let prod = proAdd('*', a, b);    
+    return [prod, `The product of ${a} and ${b} is ${prod}.`]
 }
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
-
-/////////////////////////////////////
-/* Problem 3
-
-Write a function called sumAndMultiply() that takes in three 
-numbers as separate arguments and returns an array where the 
-first element is the sum of those three numbers, the second 
-element is the product of those three numbers,  and the third 
-and fourth elements are strings that EXACTLY follow this example 
-and use the values that were input into the function:
-
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
-
-IMPORTANT DETAIL: You may not use the arithmetic 
-operators + and * in this function. To do addition, 
-use your sum() function, and to do multiplication, 
-use your multiply() function that you've already created. 
-You're going to have to be resourceful to figure out how to do this.
-
-Test this function by hand in the console to get it working, and when 
-you think it is finished, uncomment the call for the testSumAndMultiply() 
-function and see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the 
-code between GitHub and your laptop. Don't forget to create a 
-new branch for your work on the next question!
-
-*/
 
 // Write your code here
 function sumAndMultiply(a,b,c){ //eslint-disable-line
-
+    let prod = proAdd('*', a, b, c);
+    let sum = proAdd('+', a, b, c);
+    return [sum, prod, `${a} and ${b} and ${c} sum to ${sum}.`, `The product of ${a} and ${b} and ${c} is ${prod}.`]
 }
 
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
-
-
-/////////////////////////////////////
-/* Problem 4
-Write a function called sumArray() that takes in an array of 
-numbers as its single argument and then returns an array where
-the first element is the sum of the numbers in the array, and 
-the second element is a string that EXACTLY follows this example 
-and uses the values that were input into the function:
-
-"2,3,4 was passed in as an array of numbers, and 9 is their sum."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator + in this 
-function. To do addition, use your sum() function that you've already
-created. You're going to have to be resourceful to figure out how to do this.
-
-Test this function by hand in the console to get it working, and when
-you think it is finished, uncomment the call for the testSumArray() 
-function and see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the 
-code between GitHub and your laptop. Don't forget to create a 
-new branch for your work on the next question!
-*/
+testSumAndMultiply(4,7,5);
 
 // Write your code here
 var testArray = [2,3,4]; //eslint-disable-line
 
 function sumArray(testArray){ //eslint-disable-line
-
+    let sum = proAdd('+', ...testArray);
+    return [sum, `${testArray} was passed in as an array of numbers, and ${sum} is their sum.`]
 }
 
-// Here is the test for sumArray(); uncomment it to run it
-// testSumArray(testArray);
-
-
-/////////////////////////////////////
-/* Problem 5
-Write a function called multiplyArray() that takes an array of numbers
-as its argument and returns an array whose first element 
-is the product of those numbers, and the second element is 
-a string that EXACTLY follows this example and uses the values
-that were input into the function:
-
-"The numbers 2,3,4 have a product of 24."
-
-IMPORTANT DETAIL: You may not use the arithmetic operator * in this 
-function. To do multiplication, use your multiply() function that 
-you've already created. You're going to have to be resourceful to 
-figure out how to do this.
-
-Test this function by hand in the console to get it working, and 
-when you think it is finished, uncomment the call for the 
-testMultiplyArray() function and see if the test passes.
-
-Once you get the test passing, do an a-c-p cycle and synchronize the 
-code between GitHub and your laptop. Don't forget to create a 
-new branch for your work on the next question!
-*/
+testSumArray(testArray);
 
 // Write your code here
 function multiplyArray(testArray){ //eslint-disable-line
-
+    let prod = proAdd('*', ...testArray);
+    return [prod, `The numbers ${testArray} have a product of ${prod}.`];
 }
 
-// Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(2,3,4);
+testMultiplyArray(2,3,4);
